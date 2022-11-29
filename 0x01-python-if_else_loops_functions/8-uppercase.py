98bdbedae066762c7844bb6e40896be8bfd61c74
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-11-29 03:51:23 -0800

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
+        i = ord(i)
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
+            i = chr(i)
             print("{}".format(i), end="")
-        print()
+        print("")
