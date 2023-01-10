314fcf8aa7c52aaca9c8939cc8fe1d33b5e63a6b
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2023-01-10 11:45:36 -0800

    All task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-   
+    """Writes an object to a text file using JSON format"""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
