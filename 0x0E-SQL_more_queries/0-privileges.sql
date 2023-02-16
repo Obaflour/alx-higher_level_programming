e52d68a25fad8edbcf477933bbae994cf3f3ddd0
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2023-02-16 01:20:31 -0800

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This scrip lists all the privileges of the MySQL users
+-- user_0d_1 and user_0d_2 in the localhost
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
